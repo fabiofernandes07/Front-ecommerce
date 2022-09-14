4b357009de516933692f9935bb2012049793abf8
orlando <orlandoneto212@gmail.com>
2022-09-13 22:29:58 -0300

    Melhoria no component de cadastro exibindo as validações e melhoria no formulário

--- src/app/cadastro/cadastro.messages.ts
+++ src/app/cadastro/cadastro.messages.ts
@@ -4,23 +4,9 @@ import { ValidationMessages } from "src/shared/utils/validation-messages";
 
 export class CadastroMessages implements ValidateMessages {
     messages = {
-        // nome: {
-        //     required: 'Nome é obrigatório.'
-        // },
-        // sobreNome: {
-        //     required: 'Sobrenome é obrigatório.'
-        // },
-        // email: {
-        //     required: 'E-mail é obrigatório.',
-        //     emailInvalid: 'E-mail é inválido.'
-        // },
-        senha: {
-            required: 'Senha é obrigatória.',
-            passwordInvalid: 'Senha é inválida.'
+        cep: {
+            required: 'CEP é obrigatório.'
         },
-        numTelefone: {
-            phoneInvalid: 'Telefone é inválido.'
-        }
     };
     getMessages() {
         return Object.assign(new ValidationMessages().getMessages(), this.messages);
